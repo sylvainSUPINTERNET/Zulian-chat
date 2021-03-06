45272ef2f3c4189b9031261fd4e7862f18927e10
sylvainSUPINTERNET <sylvain.joly@supinternet.fr>
2021-03-06 23:50:18 +0100

    [INIT] chat ser v

---
+++ config/roomsDefintion.js
@@ -0,0 +1,17 @@
+'use strict';
+const rooms = [
+    {
+        name: "ADMIN'S ROOM",
+        description: "VIP room"
+    },
+    {
+        name: "USER'S ROOM",
+        description: "EVERYONE IN THE TEAM"
+    },
+    {
+        name: "NOOBS",
+        description: "GTFO"
+    }
+];
+
+module.exports = rooms;
---
+++ server.js
@@ -0,0 +1,60 @@
+'use strict';
+
+const app = require('express')();
+const http = require('http').Server(app);
+
+// CORS only for DEV
+const io = require('socket.io')(http, {
+    cors: {
+        origin: '*'
+    }
+});
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+
+// Configuration
+const roomsDefinition = require('./config/roomsDefintion');
+
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+// FOR DEV ONLY
+app.use(cors());
+
+
+io.on('connection', async socket => {
+    const { id, rooms } = socket;
+
+
+
+    socket.on("join", userDetailsAndRoom => {
+        const { roomName, userName } = userDetailsAndRoom;
+        socket.join(roomName);
+        io.in(roomName).emit(`welcome-user`, `Hello ${userName} !`);
+    })
+
+    // By default, user joins a room ( allow you to send a personnal message on this one)
+    console.log("New users");
+    console.log(rooms);
+
+    socket.on('disconnecting', () => {
+        console.log(socket.rooms); // the Set contains at least the socket ID
+        console.log('disconnecting')
+    });
+
+
+
+    io.on('disconnect', reason => {
+        console.log(socket.rooms.size)
+        console.log('disconnected');
+    })
+});
+
+
+
+http.listen(4001, () => {
+    console.log('listening on *:4001');
+});
